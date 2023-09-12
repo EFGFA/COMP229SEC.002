856feee617745127f66ba83201eba4e86494aedf
ziheng Huo <huo1010john@gmail.com>
2023-09-12 10:40:15 -0400

    first TRY

---
+++ Spread.js
@@ -0,0 +1,7 @@
+function userTopFriends(firstFriend, secondFriend, thirdFriends) {
+   console.log(firstFriend);
+   console.log(secondFriend);
+   console.log(thirdFriends);
+}
+var array = ['Alice', 'Bob', 'Michelle'];
+userTopFriends(...array);
---
+++ let.js
@@ -0,0 +1,9 @@
+var i =3;
+
+function iterateLet() {
+   for(let i = 0; i < 10; i++) {
+      console.log(i);
+   }
+   console.log(i);
+}
+iterateLet();
